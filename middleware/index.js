f7c3163d69b0af5313986b8eb3884a663572bd13
leecade <leecade@163.com>
2017-05-03 10:41:28 +0200

    update deps

--- middleware/index.js
+++ middleware/index.js
@@ -1,4 +1,4 @@
-import "regenerator-runtime/runtime"
+import 'regenerator-runtime/runtime'
 import devMiddleware from './devMiddleware'
 import hotMiddleware from './hotMiddleware'
 module.exports = { devMiddleware, hotMiddleware }
